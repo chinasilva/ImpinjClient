0aee217995da5ac4afc748355825f1a713ee5c53
chinasilva <www476369545@qq.com>
2016-05-28 09:07:53 +0800

    Impinj源码

---
+++ RFID源码/impinj/Install.bat
@@ -0,0 +1,4 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe D:\×ÀÃæ\impinj\WindowsServiceTest.exe
+Net Start Impinj
+sc config Impinj start= auto
+pause
---
+++ RFID源码/impinj/Uninstall.bat
@@ -0,0 +1,2 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u D:\×ÀÃæ\impinj\WindowsServiceTest.exe
+pause
